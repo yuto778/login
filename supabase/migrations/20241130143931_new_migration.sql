dffdb68474098832498b518cdfd85f4cde55c9bb
yuto778 <yuto4949@i.softbank.jp>
2024-11-30 23:52:57 +0900

    fourth

---
+++ supabase/migrations/20241130143931_new_migration.sql
@@ -0,0 +1,77 @@
+create table "public"."profiles" (
+    "id" uuid not null default auth.uid(),
+    "created_at" timestamp with time zone not null default now(),
+    "mailaddress" character varying not null,
+    "username" text not null
+);
+
+
+alter table "public"."profiles" enable row level security;
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+alter table "public"."profiles" add constraint "public_profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."profiles" validate constraint "public_profiles_id_fkey";
+
+grant delete on table "public"."profiles" to "anon";
+
+grant insert on table "public"."profiles" to "anon";
+
+grant references on table "public"."profiles" to "anon";
+
+grant select on table "public"."profiles" to "anon";
+
+grant trigger on table "public"."profiles" to "anon";
+
+grant truncate on table "public"."profiles" to "anon";
+
+grant update on table "public"."profiles" to "anon";
+
+grant delete on table "public"."profiles" to "authenticated";
+
+grant insert on table "public"."profiles" to "authenticated";
+
+grant references on table "public"."profiles" to "authenticated";
+
+grant select on table "public"."profiles" to "authenticated";
+
+grant trigger on table "public"."profiles" to "authenticated";
+
+grant truncate on table "public"."profiles" to "authenticated";
+
+grant update on table "public"."profiles" to "authenticated";
+
+grant delete on table "public"."profiles" to "service_role";
+
+grant insert on table "public"."profiles" to "service_role";
+
+grant references on table "public"."profiles" to "service_role";
+
+grant select on table "public"."profiles" to "service_role";
+
+grant trigger on table "public"."profiles" to "service_role";
+
+grant truncate on table "public"."profiles" to "service_role";
+
+grant update on table "public"."profiles" to "service_role";
+
+create policy "Enable insert for authenticated users only"
+on "public"."profiles"
+as permissive
+for insert
+to public
+with check (true);
+
+
+create policy "Enable read access for all users"
+on "public"."profiles"
+as permissive
+for select
+to public
+using (true);
+
+
+
